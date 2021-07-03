0fa11281a0bb5a126c9c19ae714ae8a9df054d07
jbruen25 <joannebruen@gmail.com>
2021-07-03 14:12:36 +0100

    Api setup for currencies and weather

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 request=requests.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=EEOAN1214S09QCDE')
 print(request.status_code)
 print(request.text)
-
+# import requests for Russia
 request=requests.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f3610f12f1cea961a8ac655c769df8a8')
 print(request.status_code)
 print(request.text)
