09e3c46a668eed9875f48f96e81906b217221de7
jbruen25 <joannebruen@gmail.com>
2021-07-03 12:36:05 +0100

    Setup project import Numpy and Pandas

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# import numpy
-import numpy as np
-# import panda
-import pandas as pd
+# import requests for USD and EUR
+import requests
+request=requests.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=EEOAN1214S09QCDE')
+print(request.status_code)
+print(request.text)
+
+
+
+
+
+
 
