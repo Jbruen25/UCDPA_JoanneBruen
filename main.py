a69c74f11425c9b66537e12ad41fc98fe5cbecb4
jbruen25 <joannebruen@gmail.com>
2021-07-11 23:04:54 +0100

    Matplotlib & Numply and analysis of data

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ print(request.text)
 
 import pandas as pd
 
+# import numpy as np
+import numpy as np
+
+
 London=pd.read_csv("London.csv")
 
 # Print the 1st 5 rows of the information
@@ -58,34 +62,103 @@ London_ind = London.set_index("House Type")
 print(London_ind)
 
 
-data=pd.read_csv("financials.csv")
+financial =pd.read_csv("financials.csv", index_col=3)
 
-print(data)
+print(financial)
 
+# summary statistics
+print(financial.head())
 
-data=pd.read_csv("Loan payments data.csv")
+print(financial.shape)
 
-print(data)
+print(financial.describe())
 
-# Print the 1st 5 rows of the information
-print(data.head())
+print(financial.info())
 
-# number of columns and rows
-print(data.shape)
+print(financial.values)
 
-# import the matplotlib.pyplot submodule and name it plt
+print(financial.columns)
+
+print(financial.index)
+
+# Sort data by sector
+financial_sector = financial.sort_values("Sector")
+
+print(financial_sector.head())
+
+# Sort data by Sector, then descending names
+financial_sector_name = financial.sort_values(["Sector", "Name"], ascending=[True, False])
+
+print(financial_sector_name.head())
+
+# select the Name, Price/Earnings, Dividend Yield, EBITDA
+
+financial_data = financial[["Name", "Price/Earnings", "Dividend Yield", "EBITDA"]]
+
+print(financial_data.head())
+
+# filter for rows where the sector is Financials and the Earnings/share is greater than 10
+
+financial_Health_100 = financial[(financial["Earnings/Share"] > 10) & (financial["Sector"] == "Financials")]
+
+print(financial_Health_100)
+
+# select the Name, Price/Earnings, Dividend Yield, EBITDA
+
+financial_data = financial[["Name", "Price/Earnings", "Dividend Yield", "EBITDA"]]
+
+# print the maximum and minimum dividend yield for financial_data
+
+print(financial_data["Dividend Yield"].max())
+
+print(financial_data["Dividend Yield"].min())
+
+# import numpy as np and create custom IQR function
+
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(.25)
+
+# Update to print IQR and median of Price/Earnings, Dividend Yield, EBITDA
+print(financial_data[["Price/Earnings", "Dividend Yield", "EBITDA"]].agg([iqr, np.median, np.min, np.max, np.mean]))
+
+# Find if there are missing values
+print(financial.isnull().sum())
+
+# Fill in the missing values with XXX
+financial_filled = financial.fillna(0)
+
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("Loan payments data.csv", index_col=3)
+data = pd.read_csv("financials.csv", index_col=2)
 
 # create a Figure and an Axes with plt.subplots
 fig, ax = plt.subplots()
 
-ax.bar(data.index, data["loan_status"])
-ax.set_xlabel('terms')
-ax.set_ylabel('loan_status')
+ax.bar(data.index, data["52 Week High"], label="52 Week High")
+ax.set_xticklabels(data.index, rotation=90)
+ax.set_ylabel('Price')
+ax.legend()
 
 plt.show()
+
+fig, ax = plt.subplots()
+
+
+ax.bar(data.index, data["52 Week Low"], label="52 Week Low")
+ax.set_xticklabels(data.index, rotation=90)
+
+ax.set_ylabel('Price')
+ax.legend()
+
+plt.show()
+
+
+
+
+
+
+
+
 
 
 
